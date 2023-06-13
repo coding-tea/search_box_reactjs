2a062ac871d5520df05aeb8863d475f114f0f3ae
digital opium <test@ismail.com>
2023-06-13 17:16:15 +0100

    project files stucture

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './app.css';
+
+function App() {
+
+  return (
+    <div className="app">
+      <header>
+        search box <a href="https://github.com/coding-tea" target='_blank' >@codingTea <i class="bi bi-github"></i></a>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
